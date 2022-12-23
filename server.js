dc2a48422437a370aaa0cb0f852a6c2e9e340b1e
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2022-12-23 21:28:38 +0530

    first

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+require('dotenv').config() 
+const PORT = process.env.PORT || 5000
+require('../backend/Config/db')
+app.use(cors())
+
+const router = require('../backend/routes')
+app.use(bodyparser.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname , '/frontend/build')));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend", "build","index.html"))
+  })
+}
+
+
+app.use(router)
+
+
+
+app.listen(PORT,()=>console.log('Listening ON PORT ', PORT))
