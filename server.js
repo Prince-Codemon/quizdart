a71d66b49c3db504cbb0e943eba61c5e70c7ce70
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2022-12-25 12:44:26 +0530

    r

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
-const path = require('path')
 const cors = require('cors')
 require('dotenv').config() 
 const PORT = process.env.PORT || 5000
@@ -13,12 +12,13 @@ app.use(bodyparser.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+app.use(router)
 app.get('/',(req,res)=>{
   res.send('Hello World')
 })
-
-app.use(router)
-
+app.get('/api/test',(req,res)=>{
+  res.send('Hello  api')
+})
 
 
 app.listen(PORT,()=>console.log('Listening ON PORT ', PORT))
