0d8904fc2c1e58ace19f87745b03e3fd57dff467
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2022-12-26 16:35:43 +0530

    new

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL,
-   {useNewUrlParser:true}).then(()=>{
+   {useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
 console.log('Connection Successful')
 }).catch((e)=>{
  console.log(e)
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
+require('dotenv').config() 
+require('../backend/Config/db')
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
 const cors = require('cors')
-require('dotenv').config() 
-require('../backend/Config/db')
 const PORT = process.env.PORT || 3001
-const router = require('../backend/routes')
+// const router = require('../backend/routes')
 app.use(cors())
 app.use(express.json())
-app.use(bodyparser.json())
-app.use(express.urlencoded({extended:true}))
-app.use(router)
+// app.use(bodyparser.json())
+// app.use(express.urlencoded({extended:true}))
+// app.use(router)
 app.get('/',(req,res)=>{
   res.send('Server Running')
 })
 
-
-
-app.listen(PORT,()=>console.log('Listening ON PORT', PORT))
+app.listen(PORT,()=>console.log('Listening ON PORT'))
